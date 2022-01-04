64db5b0cb4e0ff6eb618be4915a7e153968736fe
C <earthhominid@gmail.com>
2022-01-04 12:08:51 -0300

    filter for single member

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 const express = require('express');
 const path = require('path');
-const members = require('./members');
+const members = require('./Members');
 const logger = require('./middleware/logger');
-
 const app = express();
 
 // Init middleware
 // app.use(logger);
 
-// Gets all members
-app.get('/api/members', (req, res) => res.json(members));
+// Gets All members
+app.get('/api/members', (req, res) => {
+  res.json(members);
+});
 
 // Get single member
 app.get('/api/members/:id', (req, res) => {
-  // If exists, found is true, if doesn't exist, found equals false
-  const found = members.some(
-    (member) => member.id === parseInt(req.paramams.id)
-  );
-  if (found) {
-    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({ msg: `No member with ID of ${req.params.id}` });
-  }
+  res.json(members.filter((member) => member.id === parseInt(req.params.id)));
 });
 
 // Set static folder
@@ -29,6 +22,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}!`);
-});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
